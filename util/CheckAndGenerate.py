b70d4235702c2292d9bcfa9588034c0bce51853d
MorriganR <dasha@ddyka.com>
2022-08-02 01:20:07 -0700

    add pdfkit

--- util/CheckAndGenerate.py
+++ util/CheckAndGenerate.py
@@ -1,32 +1,59 @@
 import os
+import markdown
+import pdfkit
 
-testDocFileName = './temp/test.doc'
+testHtmlFileName = './temp/test.html'
 testPdfFileName = './temp/test.pdf'
 
+testStyle = """<style>
+table {
+    border-collapse: collapse;
+}
+table, th, td {
+   border: 1px solid black;
+}
+</style>"""
+
+pdfKitOptions = {
+    'page-size': 'A4',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '1.00in',
+    'encoding': "UTF-8"
+}
+
 try:
-    os.remove(testDocFileName)
+    os.remove(testHtmlFileName)
     os.remove(testPdfFileName)
 except OSError:
     pass
 
-testDoc = open(testDocFileName, 'a', encoding='utf-8')
-testPdf = open(testPdfFileName, 'a', encoding='utf-8')
+testHtml = open(testHtmlFileName, 'a', encoding='utf-8')
+# testPdf = open(testPdfFileName, 'a', encoding='utf-8')
+
+# set html charset
+# testHtml.write("""<meta http-equiv="Content-type" content="text/html; charset=utf-8" />""")
+testHtml.write(testStyle)
 
 coursesDetaisPath = './Bachalor/Computer Engineering/details'
 coursesList = os.listdir(coursesDetaisPath)
 # print('\n'.join(coursesList))
 for course in coursesList:
-    courseDetailFileName = coursesDetaisPath + '/' + course + '/README.md'
+    # courseDetailFileName = coursesDetaisPath + '/' + course + '/README.md'
     courseDetailUaFileName = coursesDetaisPath + '/' + course + '/README_UA.md'
     if os.stat(courseDetailUaFileName).st_size > 42:
         print(courseDetailUaFileName + ' - ' + str(os.stat(courseDetailUaFileName).st_size))
-        courseDetail = open(courseDetailFileName, 'r', encoding='utf-8')
+        # courseDetail = open(courseDetailFileName, 'r', encoding='utf-8')
         courseDetailUa = open(courseDetailUaFileName, 'r', encoding='utf-8')
-        courseDetail.close()
+        testHtml.write(markdown.markdown(courseDetailUa.read(), extensions=['tables'], output_format='html'))
+        # courseDetail.close()
         courseDetailUa.close()
 
-testDoc.write("test to Doc\n")
-testPdf.write("test to Pdf\n")
 
-testDoc.close()
-testPdf.close()
+# testPdf.write("test to Pdf\n")
+
+testHtml.close()
+# testPdf.close()
+
+pdfkit.from_file(testHtmlFileName, testPdfFileName, options=pdfKitOptions)
